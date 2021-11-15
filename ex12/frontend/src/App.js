4677c68fe160ef198ec97dba7ed32c50fbc50823
Tapio Salonen <tsa.dom@pm.me>
2021-11-15 21:35:08 +0200

    Exercise 12

---
+++ ex12/frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import React from 'react'
+
+const App = () => {
+
+  return (
+    <>
+      <div>Hello world</div>
+      <img alt="Wonderful" src="http://localhost:8080/api/image" style={{ width: 500, height: 500}} />
+    </>
+  )
+}
+
+export default App;
